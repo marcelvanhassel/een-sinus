61bceeda5d50b7d71edbfbae11e92da3abc87bc1
Marcel van Hassel <marcel@mt183.hku.nl>
2013-11-26 15:25:41 +0100

    Eerste commit, werkende versie, poept 100 getallen uit voor een sinus

---
+++ eensinus.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	// Variabele t, tijd voor de sinus
+	double t;
+	
+	// Telt in 100 stapjes van 0 tot 2
+	for(t = 0.0 ; t <= 2.0; t += 0.02)
+	{
+		// Sinus berekend
+		double result = sin(t);
+
+		cout << result << endl;
+
+	}	
+
+	return 0;
+}
